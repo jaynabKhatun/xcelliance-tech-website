c2e0d92f94527ff44d9b3e37016692df224f33e4
unknown <anisamim94@gmail.com>
2024-06-07 00:37:41 +0600

    added product ditails page

--- src/Pages/ProductsDetails/ProductsDetailsPages.jsx
+++ src/Pages/ProductsDetails/ProductsDetailsPages.jsx
@@ -4,6 +4,9 @@ import paralax from '../../../src/assets/bg/bg.jpg'
 import { useParams } from 'react-router-dom';
 import useAxiosCommon from '../../Hooks/UseAxiosCommon';
 import { BiSolidLike } from 'react-icons/bi';
+import { FaStar } from 'react-icons/fa';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
 
 
 const ProductsDetailsPages = () => {
@@ -21,28 +24,119 @@ const ProductsDetailsPages = () => {
 
     })
 
-    return (
-        <div className='h-[700px] bg-fixed opacity-80' style={{
-            backgroundImage: `url(${paralax})`,
-        }}>
-            <div>
-                <div className="text-white flex justify-center items-center" >
 
-                    <section>
-                        <div className="dark:bg-violet-600">
-                            <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-50">
-                                <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-50">{products.name}</h1>
-                                <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-50">{products.description}!</p>
+    const handleReview = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+
+        const rating = form.rating.value;
+        const review = form.review.value;
+        const reviewData = { name, email, rating, review }
+        console.log(reviewData)
+
+    }
+
+    // <div className='h-[700px]  opacity-80  mb-96' style={{
+    //     backgroundImage: `url(${paralax})`,
+    // }}>
+
+    return (
+        <div className="p-5 mx-auto sm:p-10 md:p-16 dark:bg-gray-100 dark:text-gray-800 ">
+            <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded mt-32">
+                <img src={products.image} alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500" />
+                <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-50">
+                    <div className="space-y-2">
+                        <a rel="noopener noreferrer" href="#" className="inline-block bg-base-300 mb-10 p-4 text-2xl font-semibold sm:text-3xl">{products.name} <div className="badge badge-secondary">{products.category}</div>
+                        </a>
+
+                        <p className=" dark:text-gray-600  font-jost text-2xl">{
+                            products.description
+                        }
+
+                        </p>
+                        <p>
+                            {products.tags?.map((tag, index) => (
+                                <p key={index}># {tag}</p>
+                            ))}
+                        </p>
+                        <p>
+                            { }
+                        </p>
+
+                    </div>
+
+
+                    <div className='flex justify-end gap-6'>
+                        <button className="btn btn-outline btn-secondary border-blue-600">{products.upvotes}<BiSolidLike className='text-4xl' />
+                        </button>
+                        <button className="btn btn-outline btn-secondary border-blue-600">Report This Product</button>
+                    </div>
+
+                    <h1 className='text-4xl font-jost'>
+                        Write a Review
+                    </h1>
+
+
+                    <form onSubmit={handleReview}>
+                        <div className="grid grid-cols-4 gap-6 border-blue-600 border-2 p-6 rounded-md shadow-sm dark:bg-gray-50">
+
+                            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                                <div className="col-span-full sm:col-span-3">
+                                    <label className="text-sm">Username</label>
+                                    <input id="username" name='name' type="text" placeholder="Username" className="w-full border p-2 border-blue-600 rounded-md" />
+                                </div>
+                                <div className="col-span-full sm:col-span-3">
+                                    <label className="text-sm">email</label>
+                                    <input type="text" name='email' placeholder="email" className="w-full p-2 border border-blue-600 rounded-md" />
+                                </div>
+                                <div className="col-span-full sm:col-span-3">
+                                    <label className="text-sm">Rating</label>
+                                    <input id="rating" type="text" name='rating' className="w-full p-2 border border-blue-600 rounded-md" />
+                                </div>
+                                <div className="col-span-full">
+                                    <label className="text-sm">write a Review</label>
+                                    <textarea id="bio" placeholder="Review" name='review' className="w-full  border border-blue-600 rounded-md"></textarea>
+                                </div>
+                                <input className='btn btn-primary w-40 ' type="submit" value="Submit Review" />
+
 
                             </div>
+
                         </div>
-                        <img src={products.image} alt="" className="w-5/6 mx-auto mb-12 -mt-20 dark:bg-gray-500 rounded-lg shadow-md lg:-mt-40" />
-                    </section>
+
+                    </form>
+
 
                 </div>
             </div>
 
+            <SectionTitle subHeading='Our Customer' heading=' Review '></SectionTitle>
+            <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md dark:divide-gray-300 dark:bg-gray-50 dark:text-gray-800">
+                <div className="flex justify-between p-4">
+                    <div className="flex space-x-4">
+                        <div>
+                            <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                        </div>
+                        <div>
+                            <h4 className="font-bold">Leroy Jenkins</h4>
+                            <span className="text-xs dark:text-gray-600">2 days ago</span>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-2 dark:text-yellow-700">
+                        <FaStar></FaStar>
+                        <span className="text-xl font-bold">4.5</span>
+                    </div>
+                </div>
+                <div className="p-4 space-y-2 text-sm dark:text-gray-600">
+                    <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+                    <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+                </div>
+            </div>
         </div>
+
+
     );
 };
 
