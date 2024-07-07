2516a685dd51aa6ec32d65a5de1af44cc9ad40e1
unknown <anisamim94@gmail.com>
2024-07-07 14:52:43 +0600

    added footer with require info

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,26 +1,57 @@
 import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
+import logo from "../../../public/logo.png"
 
 const Footer = () => {
-    return (
-        <footer className="footer footer-center p-10 mt-16  bg-base-200 text-base-content rounded">
-            <nav className="grid grid-flow-col gap-4">
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <div className="grid grid-flow-col gap-4">
-                    <FaTwitter className="text-2xl" />
-                    <FaFacebook className="text-2xl"></FaFacebook>
-                    <FaYoutube className="text-2xl"></FaYoutube>
-                </div>
-            </nav>
-            <aside>
-                <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
-            </aside>
-        </footer>
-    );
+  return (
+    <div>
+      <footer className="footer bg-base-200 text-base-content p-10 mt-10">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+      <footer className="footer bg-black text-base-content border-base-300 border-t px-10 py-4">
+        <aside className="grid-flow-col items-center gap-4">
+          <img className="w-20" src={logo} alt="" />
+
+          <p className="text-white text-xs">
+            xcellinace Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </aside>
+        <nav className="md:place-self-center md:justify-self-end">
+          <div className="flex">
+            <a>
+              <FaTwitter className="w-24 h-8 text-blue-600"></FaTwitter>
+            </a>
+            <a>
+              <FaYoutube className="w-24 h-8 text-red-700"></FaYoutube>
+            </a>
+            <a> 
+             <FaFacebook className="w-24 h-8 text-blue-600"></FaFacebook>
+            </a>
+          </div>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
