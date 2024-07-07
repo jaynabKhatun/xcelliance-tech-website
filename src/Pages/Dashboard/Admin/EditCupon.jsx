976ec1ced9111ec52be1409afe9c810bdbd567e7
unknown <anisamim94@gmail.com>
2024-07-07 14:09:05 +0600

    added cupon edit

--- src/Pages/Dashboard/Admin/EditCupon.jsx
+++ src/Pages/Dashboard/Admin/EditCupon.jsx
@@ -1,25 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { useParams } from "react-router-dom";
+
+import { useNavigate, useParams } from "react-router-dom";
 import useAxiosSecure from "../../../Hooks/UseAxiosSecure";
+import Swal from "sweetalert2";
 
 ("react-router-dom");
 
 const EditCupon = () => {
   const axioSecure = useAxiosSecure();
   const { id } = useParams();
+  const navigate = useNavigate();
   //   console.log(id);
 
-  const { data: cupons } = useQuery({
-    queryKey: ["cupons", id],
+  const { data: cupon } = useQuery({
+    queryKey: ["cupon", id],
     queryFn: async () => {
-      const res = await axioSecure.get(`/cupons/${id}`);
-      console.log(res.data);
-    //   return res.data;
+      const res = await axioSecure.get(`/cupon/${id}`);
+      return res?.data;
     },
   });
+  console.log(cupon);
 
-  const handleEditCupon = (e) => {
+  const handleEditCupon = async (e) => {
     console.log("press");
     e.preventDefault();
     const form = e.target;
@@ -29,6 +31,19 @@ const EditCupon = () => {
     const date = form.date.value;
     const newCupon = { name, CuponCode, discount, date };
     console.log(newCupon);
+
+    //update cupon from db
+    const res = await axioSecure.put(`/cupon/${id}`, newCupon);
+    console.log(res.data);
+    if (res.data.modifiedCount === 1) {
+      Swal.fire({
+        icon: "success",
+        title: "Cupon Updated",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      navigate('/Dashboard/manageCupon');
+    }
   };
 
   return (
@@ -42,7 +57,7 @@ const EditCupon = () => {
             type="text"
             name="name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            defaultValue={cupons.name}
+            defaultValue={cupon?.name}
             required
           />
           <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
@@ -55,7 +70,7 @@ const EditCupon = () => {
             type="text"
             name="discount"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            defaultValue={cupons.discount}
+            defaultValue={cupon?.discount}
             required
           />
           <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
@@ -67,7 +82,7 @@ const EditCupon = () => {
             type="text"
             name="CuponCode"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            defaultValue={cupons.CuponCode}
+            defaultValue={cupon?.CuponCode}
             required
           />
           <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
@@ -80,7 +95,7 @@ const EditCupon = () => {
             type="date"
             name="date"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            defaultValue={cupons.date}
+            defaultValue={cupon?.date}
             required
           />
           <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
